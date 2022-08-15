806320ca8573013f7a8f9659aaf3557ee1bacb26
luizhenriquelongo <longo.luizh@gmail.com>
2022-08-15 18:01:39 -0300

    feat(pyiso3166): enable subdivisions on the package

--- src/pyiso3166/__init__.py
+++ src/pyiso3166/__init__.py
@@ -1,4 +1,6 @@
 from countries import Countries
+from subdivisions import Subdivisions
 from core import ISOCodes
 
 countries = Countries(ISOCodes.i3166_1)
+subdivisions = Subdivisions(ISOCodes.i3166_2)
